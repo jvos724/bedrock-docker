95282525dd684c62ad2f706a6ce3faf7f3e3a220
Jeff Vos <bugs_slights0t@icloud.com>
2024-07-30 16:33:29 -0400

    Locked in specific versions of Bedrock & Sage

--- dev.sh
+++ dev.sh
@@ -1,2 +1,7 @@
-git clone git@github.com:roots/bedrock bedrock
-git clone git@github.com:roots/sage bedrock/web/app/themes/sage
+# Clone Bedrock: WordPress 6.6.1
+git clone --depth 1 --branch "1.24.4" git@github.com:roots/bedrock bedrock
+# git clone git@github.com:roots/bedrock bedrock
+
+# Clone Sage v10.8.2
+git clone --depth 1 --branch "v10.8.2" git@github.com:roots/sage bedrock/web/app/themes/sage
+# git clone git@github.com:roots/sage bedrock/web/app/themes/sage
